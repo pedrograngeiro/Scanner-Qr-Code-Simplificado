cd9b66afa5de316e4e688360ffef7b76ad5a1194
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2020-11-08 19:54:04 -0300

    Update decode.py

--- decode.py
+++ decode.py
@@ -4,13 +4,16 @@ import pyzbar.pyzbar as pyzbar
 
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_PLAIN
-
+condicao = 0
 while True:
     _, frame = cap.read()
 
     decodedObjects = pyzbar.decode(frame)
     for obj in decodedObjects:
-        #print("Data", obj.data)
+        if condicao = 0:
+            print("Data", obj.data)
+            condicao = condicao + 1
+        
         cv2.putText(frame, str(obj.data), (50, 50), font, 2,
                     (255, 0, 0), 3)
 
